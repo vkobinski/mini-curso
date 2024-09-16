1969183963182b20e375528d35d657a47fe6aa15
vkobinski <victorkobinski@outlook.com>
2024-09-16 12:56:02 -0300

    First

--- src/main/java/br/uepg/mini_curso/controllers/MensagemController.java
+++ src/main/java/br/uepg/mini_curso/controllers/MensagemController.java
@@ -1,4 +1,16 @@
 package br.uepg.mini_curso.controllers;
 
+import br.uepg.mini_curso.models.Mensagem;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("mensagem")
 public class MensagemController {
+
+    @GetMapping(name = "/{id}", produces = "application/json")
+    public @ResponseBody Mensagem getMensagem(@PathVariable Long id) {
+        return new Mensagem();
+    }
+
 }
