1039838859c41d54f7075dbe35f79d6665b27512
vkobinski <victorkobinski@outlook.com>
2024-09-16 12:50:07 -0300

    First

--- src/main/java/br/uepg/mini_curso/models/Mensagem.java
+++ src/main/java/br/uepg/mini_curso/models/Mensagem.java
@@ -1,4 +1,18 @@
 package br.uepg.mini_curso.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Mensagem {
+    @Id
+    private Long id;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
 }
