cf87e8366545725971a1217f5773794df5af188f
vkobinski <victorkobinski@outlook.com>
2024-09-16 12:22:15 -0300

    First

---
+++ src/main/java/br/uepg/mini_curso/MiniCursoApplication.java
@@ -0,0 +1,13 @@
+package br.uepg.mini_curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniCursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniCursoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/uepg/mini_curso/MiniCursoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.uepg.mini_curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniCursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
