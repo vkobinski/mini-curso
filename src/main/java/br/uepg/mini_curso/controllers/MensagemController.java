a8d38fcc8c9a5775babd4b5e821de48d468c7763
vkobinski <victorkobinski@outlook.com>
2024-09-16 22:22:39 -0300

    First

--- src/main/java/br/uepg/mini_curso/controllers/MensagemController.java
+++ src/main/java/br/uepg/mini_curso/controllers/MensagemController.java
@@ -1,16 +1,30 @@
 package br.uepg.mini_curso.controllers;
 
 import br.uepg.mini_curso.models.Mensagem;
+import br.uepg.mini_curso.repository.MensagemRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("mensagem")
+@RequestMapping("/mensagem")
 public class MensagemController {
 
-    @GetMapping(name = "/{id}", produces = "application/json")
-    public @ResponseBody Mensagem getMensagem(@PathVariable Long id) {
-        return new Mensagem();
+    private final MensagemRepository repo;
+
+    public MensagemController(MensagemRepository repo) {
+        this.repo = repo;
+    }
+
+    @GetMapping(path = "/{id}")
+    public @ResponseBody String getMensagem(@PathVariable Long id) {
+        return repo.getById(id).getCorpo();
+    }
+
+    @GetMapping(path = "/cria/{mensagem}")
+    public @ResponseBody String criaMensagem(@PathVariable String mensagem) {
+        Mensagem nova_mensagem = new Mensagem(mensagem);
+        repo.save(nova_mensagem);
+        return "OK";
     }
 
 }
--- src/main/java/br/uepg/mini_curso/models/Mensagem.java
+++ src/main/java/br/uepg/mini_curso/models/Mensagem.java
@@ -1,12 +1,33 @@
 package br.uepg.mini_curso.models;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 
 @Entity
 public class Mensagem {
+
+    public Mensagem() {}
+
+    public Mensagem(String corpo) {
+        this.corpo = corpo;
+    }
+
     @Id
+    @GeneratedValue
     private Long id;
+
+    @Column
+    private String corpo;
+
+    public String getCorpo() {
+        return corpo;
+    }
+
+    public void setCorpo(String corpo) {
+        this.corpo = corpo;
+    }
 
     public void setId(Long id) {
         this.id = id;
